21caae3ed515919bc88c4b254aa56a84bd9af29a
teacher <dakcorp@3g.ua>
2014-12-01 19:56:24 +0200

    add modal window for new product

--- www/admin/index.php
+++ www/admin/index.php
@@ -31,114 +31,51 @@
     <script type="text/javascript" src="../js/bootstrap-datetimepicker.min.js"></script>
     <script type="text/javascript" src="../js/bootstrap-datetimepicker.ru.js"></script>
     <style type="text/css">
-		.tabs-below > .nav-tabs,
-		.tabs-right > .nav-tabs,
-		.tabs-left > .nav-tabs {
-		  border-bottom: 0;
-		}
-
-		.tab-content > .tab-pane,
-		.pill-content > .pill-pane {
-		  display: none;
-		}
-
-		.tab-content > .active,
-		.pill-content > .active {
-		  display: block;
-		}
-
-		.tabs-below > .nav-tabs {
-		  border-top: 1px solid #ddd;
-		}
-
-		.tabs-below > .nav-tabs > li {
-		  margin-top: -1px;
-		  margin-bottom: 0;
-		}
-
-		.tabs-below > .nav-tabs > li > a {
-		  -webkit-border-radius: 0 0 4px 4px;
-		     -moz-border-radius: 0 0 4px 4px;
-		          border-radius: 0 0 4px 4px;
-		}
-
-		.tabs-below > .nav-tabs > li > a:hover,
-		.tabs-below > .nav-tabs > li > a:focus {
-		  border-top-color: #ddd;
-		  border-bottom-color: transparent;
-		}
-
-		.tabs-below > .nav-tabs > .active > a,
-		.tabs-below > .nav-tabs > .active > a:hover,
-		.tabs-below > .nav-tabs > .active > a:focus {
-		  border-color: transparent #ddd #ddd #ddd;
-		}
-
-		.tabs-left > .nav-tabs > li,
-		.tabs-right > .nav-tabs > li {
-		  float: none;
-		}
-
-		.tabs-left > .nav-tabs > li > a,
-		.tabs-right > .nav-tabs > li > a {
-		  min-width: 74px;
-		  margin-right: 0;
-		  margin-bottom: 3px;
-		}
-
-		.tabs-left > .nav-tabs {
-		  float: left;
-		  margin-right: 19px;
-		  border-right: 1px solid #ddd;
-		}
-
-		.tabs-left > .nav-tabs > li > a {
-		  margin-right: -1px;
-		  -webkit-border-radius: 4px 0 0 4px;
-		     -moz-border-radius: 4px 0 0 4px;
-		          border-radius: 4px 0 0 4px;
-		}
-
-		.tabs-left > .nav-tabs > li > a:hover,
-		.tabs-left > .nav-tabs > li > a:focus {
-		  border-color: #eeeeee #dddddd #eeeeee #eeeeee;
-		}
-
-		.tabs-left > .nav-tabs .active > a,
-		.tabs-left > .nav-tabs .active > a:hover,
-		.tabs-left > .nav-tabs .active > a:focus {
-		  border-color: #ddd transparent #ddd #ddd;
-		  *border-right-color: #ffffff;
-		}
-
-		.tabs-right > .nav-tabs {
-		  float: right;
-		  margin-left: 19px;
-		  border-left: 1px solid #ddd;
-		}
-
-		.tabs-right > .nav-tabs > li > a {
-		  margin-left: -1px;
-		  -webkit-border-radius: 0 4px 4px 0;
-		     -moz-border-radius: 0 4px 4px 0;
-		          border-radius: 0 4px 4px 0;
-		}
-
-		.tabs-right > .nav-tabs > li > a:hover,
-		.tabs-right > .nav-tabs > li > a:focus {
-		  border-color: #eeeeee #eeeeee #eeeeee #dddddd;
-		}
-
-		.tabs-right > .nav-tabs .active > a,
-		.tabs-right > .nav-tabs .active > a:hover,
-		.tabs-right > .nav-tabs .active > a:focus {
-		  border-color: #ddd #ddd #ddd transparent;
-		  *border-left-color: #ffffff;
-		}
-
+    	.no_width_resize {
+    		resize:vertical;
+    	}
     </style>
 </head>
 <body>
+
+<!-- Modal addProduct -->
+<div class="modal fade" id="addProduct" tabindex="-1" role="dialog" aria-labelledby="addProductLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="addProductLabel">Новый товар</h4>
+            </div>
+            <div class="modal-body">
+                <form id="loginFromModal" action="autorizetion.php" method="POST">
+                    <div class="form-group">
+                        <label>Наименвание товара</label>
+                        <input name="productName" type="text" class="form-control" placeholder="Product Name">
+                    </div>
+                    <div class="row">
+	                    <div class="form-group col-md-6">
+	                        <label>Категория</label>
+	                        <input name="productCategory" type="text" class="form-control" placeholder="Category">
+	                    </div>
+	                    <div class="form-group col-md-4 pull-right">
+	                        <label>Цена</label>
+	                        <input name="productPrice" type="text" class="form-control" placeholder="Price">
+	                    </div>
+	                </div>
+                    <div class="form-group">
+                        <label>Описание товара</label>
+                        <textarea name="productDescription" class="form-control no_width_resize" rows="5"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button form="loginFromModal" type="submit" class="btn btn-primary">Add</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 	<!-- navigation bar -->
 	<div class="navbar navbar-inverse navbar-fixed-top">
 	    <div class="container">
@@ -171,19 +108,45 @@
 			    <div class="tabbable tabs-left">
     				<ul class="nav nav-tabs">
       					<li><a href="#a" data-toggle="tab">One</a></li>
-      					<li class="active"><a href="#b" data-toggle="tab">Two</a></li>
-      					<li><a href="#c" data-toggle="tab">Twee</a></li>
+      					<li><a href="#b" data-toggle="tab">Two</a></li>
+      					<li class="active"><a href="#productsTab" data-toggle="tab">Товары</a></li>
     				</ul>
     				<div class="tab-content">
-     					<div class="tab-pane active" id="a">Lorem ipsum dolor sit amet, charetra varius quam sit amet vulputate. 
+     					<div class="tab-pane" id="a">Lorem ipsum dolor sit amet, charetra varius quam sit amet vulputate. 
      						Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero.
      					</div>
      					<div class="tab-pane" id="b">Secondo sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
      						Aliquam in felis sit amet augue.
      					</div>
-     					<div class="tab-pane" id="c">Thirdamuno, ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-     						Quisque mauris augue, molestie tincidunt condimentum vitae. 
+
+     					<!--  TAB PRODUCTS  -->
+     					<div class="tab-pane active" id="productsTab">
+     						<div class="row">
+     							<div class="col-md-3">
+     								<a class="btn btn-link" href="#" data-toggle="modal" data-target="#addProduct">
+     									<span class="glyphicon glyphicon-plus"></span> Добавить товар
+     								</a>
+     							</div>
+
+     							<div class="col-md-12">
+		     						<table class="table table-hover">
+		     							<thead>
+		     								<tr>
+		     									<th>#</th>
+		     									<th>Название</th>
+		     									<th>Описание</th>
+		     									<th>Категория</th>
+		     									<th>Цена</th>
+		     								</tr>
+		     							</thead>
+		     							<tbody id="productsList">
+		     								<!-- There some place for products -->
+		     							</tbody>
+		     						</table>
+		     					</div>
+							</div>
      					</div>
+						<!--  end TAB PRODUCTS  -->
     				</div>
   				</div>
 			</div>
